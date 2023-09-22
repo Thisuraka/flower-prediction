507bfc9fa8db89a5cd7c37e5f917fc923daab649
Thisuraka <vibudathisuraka@email.com>
2023-09-22 11:22:21 +0530

    helper images

---
+++ lib/style.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+const inactiveColor = Color(0xFFCFCFCF);
+const warningAlertColor = Color(0xFFFF7F50);
+const successAlertColor = Color(0xFF009650);
+const errorAlertColor = Color(0xFFFF3434);
+const loadingPlaceholderColor = Color(0xFFE0E0E0);
+
+const greenLvl1 = Color(0xFF71AB95);
+const greenLvl2 = Color(0xFF7FB3A0);
+const greenLvl3 = Color(0xFF8DBCAA);
+const greenLvl4 = Color(0xFF9CC4B5);
+const greenLvl5 = Color(0xFFAACDBF);
+const greenLvl6 = Color(0xFFB8D5CA);
+const greenLvl7 = Color(0xFFC6DDD5);
+const greenLvl8 = Color(0xFFD4E6DF);
+
+//Fonts and text
+const labelStyle = TextStyle(color: Colors.black26, fontWeight: FontWeight.w500, fontSize: 15.0);
+
+const hintStyle = TextStyle(color: Colors.black26, fontWeight: FontWeight.w500, fontSize: 13.0);
+
+const topicStyle = TextStyle(color: Colors.black87, fontWeight: FontWeight.w500, fontSize: 25.0);
