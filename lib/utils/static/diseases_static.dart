88ccf9ad0746b0f92173f756e7b7ee4efcd250ef
Sandaru Abeyratne <sandaru.abeyratne@gmail.com>
2023-10-24 09:39:40 +0530

    Static files updated for disease and growth

--- lib/utils/static/diseases_static.dart
+++ lib/utils/static/diseases_static.dart
@@ -71,284 +71,296 @@ List<DiseaseModel> diseaseStatic = [
         "Remember that early intervention is critical in managing black rot disease effectively. Implementing these measures in stage 2 can help control the disease's spread and minimize its impact on your crops.",
     treatmentPlan: {
       0: TreatmentPlan(
-        plan: "",
-        desc: "",
+        plan: "Prune Affected Leaves",
+        desc:
+            "Begin by removing and destroying (do not compost) all leaves and plant material showing symptoms of black rot. This helps reduce the disease's spread",
       ),
       1: TreatmentPlan(
-        plan: "",
-        desc: "",
+        plan: "Sanitation",
+        desc:
+            "Keep the planting area clean and free from plant debris, as the pathogen can survive on fallen leaves and other plant materials",
       ),
       2: TreatmentPlan(
-        plan: "",
-        desc: "",
+        plan: "Crop Rotation",
+        desc:
+            "Avoid planting cruciferous crops in the same location for several years to break the disease cycle.",
       ),
       3: TreatmentPlan(
-        plan: "",
-        desc: "",
+        plan: "Plant Resistant Varieties",
+        desc:
+            "Whenever possible, select plant varieties that are resistant to black rot.",
       ),
       4: TreatmentPlan(
-        plan: "",
-        desc: "",
+        plan: "Fungicides",
+        desc:
+            "Consider using fungicides labeled for black rot control. Follow the manufacturer's instructions for application and safety precautions.",
       ),
       5: TreatmentPlan(
-        plan: "",
-        desc: "",
+        plan: "Spacing and Pruning",
+        desc:
+            "Properly space plants to allow for good air circulation, and prune to reduce leaf density, which can slow the disease's spread.",
       ),
       6: TreatmentPlan(
-        plan: "",
-        desc: "",
+        plan: "Water Management",
+        desc:
+            "Water at the base of the plants to prevent moisture on the leaves. Overhead irrigation can encourage disease spread.",
       ),
       7: TreatmentPlan(
-        plan: "",
-        desc: "",
+        plan: "Soil Health",
+        desc:
+            "Ensure proper soil drainage and maintain optimal soil pH for your crops. Healthy plants are better equipped to resist disease.",
       ),
       8: TreatmentPlan(
-        plan: "",
-        desc: "",
+        plan: "Crop Monitoring",
+        desc:
+            "Regularly inspect plants for any signs of new infections, and take action promptly if you notice symptoms.",
       ),
       9: TreatmentPlan(
-        plan: "",
-        desc: "",
+        plan: "Consult with Local Experts",
+        desc:
+            "Reach out to local agricultural extension services or experts for advice specific to your region, as the severity and timing of black rot can vary based on local conditions.",
       ),
     },
   ),
   DiseaseModel(
-    key: "Blight_Stage_1",
+    key: "Scab_leaf_disease_Stage_1",
     disease: "Blight leaf disease stage 1",
-    description: "",
+    description:
+        "In the first stage of scab leaf disease, early symptoms become visible. Infected leaves display small, superficial lesions or scabs, often with a velvety or powdery appearance, which give the disease its name. These lesions might be slightly discolored and do not yet significantly harm the overall health of the tree. Stage 1 is a crucial early detection point, signaling the need for vigilant orchard management to prevent the disease from advancing and causing more severe damage to the flowers and leaves.",
     description2: "",
     treatmentPlan: {
       0: TreatmentPlan(
-        plan: "",
-        desc: "",
+        plan: "Prune and Remove Affected Leaves",
+        desc:
+            "Begin by carefully pruning and removing leaves showing early scab symptoms. Make sure to dispose of the infected leaves properly, as they can harbor the pathogen.",
       ),
       1: TreatmentPlan(
-        plan: "",
-        desc: "",
+        plan: "Sanitation",
+        desc:
+            "Maintain good orchard hygiene by regularly cleaning up fallen leaves and debris, as scab spores can overwinter on these materials.",
       ),
       2: TreatmentPlan(
-        plan: "",
-        desc: "",
+        plan: "Fungicide Application",
+        desc:
+            "Consider applying a preventive fungicide treatment in the spring before the disease becomes more severe. Consult with a local agricultural extension service or expert to determine the most appropriate fungicide and application timing for your region and tree variety.",
       ),
       3: TreatmentPlan(
-        plan: "",
-        desc: "",
+        plan: "Proper Pruning",
+        desc:
+            "Ensure good air circulation within the canopy through selective pruning. Pruning encourages faster leaf drying, making it less favorable for the disease.",
       ),
       4: TreatmentPlan(
-        plan: "",
-        desc: "",
+        plan: "Mulch and Weed Control",
+        desc:
+            "Apply mulch around the base of the trees to prevent soil splashing onto the leaves, which can spread the disease. Keep the orchard area weed-free to reduce the risk of scab spores harboring in nearby plants.",
       ),
       5: TreatmentPlan(
-        plan: "",
-        desc: "",
+        plan: "Timely Watering",
+        desc:
+            "Water your fruit trees at the base rather than overhead to minimize leaf wetness, which can contribute to disease development.",
       ),
       6: TreatmentPlan(
-        plan: "",
-        desc: "",
+        plan: "Varietal Resistance",
+        desc:
+            "If possible, consider planting or grafting scab-resistant fruit tree varieties, as they are less susceptible to the disease.",
       ),
       7: TreatmentPlan(
-        plan: "",
-        desc: "",
+        plan: "Monitor and Scout",
+        desc:
+            "Regularly inspect your trees for any signs of new infections. Early detection allows for prompt action.",
       ),
       8: TreatmentPlan(
-        plan: "",
-        desc: "",
+        plan: "Record Keeping",
+        desc:
+            "Maintain records of disease incidence and treatment schedules, which can help in planning for future seasons.",
       ),
       9: TreatmentPlan(
-        plan: "",
-        desc: "",
+        plan: "Consult Experts",
+        desc:
+            "Seek guidance from local horticultural experts or agricultural extension services for advice specific to your region and tree type.",
       ),
     },
   ),
   DiseaseModel(
-    key: "Blight_Stage_2",
-    disease: "Blight leaf disease stage 2",
-    description: "",
-    description2: "",
-    treatmentPlan: {
-      0: TreatmentPlan(
-        plan: "",
-        desc: "",
-      ),
-      1: TreatmentPlan(
-        plan: "",
-        desc: "",
-      ),
-      2: TreatmentPlan(
-        plan: "",
-        desc: "",
-      ),
-      3: TreatmentPlan(
-        plan: "",
-        desc: "",
-      ),
-      4: TreatmentPlan(
-        plan: "",
-        desc: "",
-      ),
-      5: TreatmentPlan(
-        plan: "",
-        desc: "",
-      ),
-      6: TreatmentPlan(
-        plan: "",
-        desc: "",
-      ),
-      7: TreatmentPlan(
-        plan: "",
-        desc: "",
-      ),
-      8: TreatmentPlan(
-        plan: "",
-        desc: "",
-      ),
-      9: TreatmentPlan(
-        plan: "",
-        desc: "",
-      ),
-    },
-  ),
-  DiseaseModel(
-    key: "Healthy",
-    disease: "Healthy",
-    description: "",
-    description2: "",
-    treatmentPlan: {
-      0: TreatmentPlan(
-        plan: "",
-        desc: "",
-      ),
-      1: TreatmentPlan(
-        plan: "",
-        desc: "",
-      ),
-      2: TreatmentPlan(
-        plan: "",
-        desc: "",
-      ),
-      3: TreatmentPlan(
-        plan: "",
-        desc: "",
-      ),
-      4: TreatmentPlan(
-        plan: "",
-        desc: "",
-      ),
-      5: TreatmentPlan(
-        plan: "",
-        desc: "",
-      ),
-      6: TreatmentPlan(
-        plan: "",
-        desc: "",
-      ),
-      7: TreatmentPlan(
-        plan: "",
-        desc: "",
-      ),
-      8: TreatmentPlan(
-        plan: "",
-        desc: "",
-      ),
-      9: TreatmentPlan(
-        plan: "",
-        desc: "",
-      ),
-    },
-  ),
-  DiseaseModel(
-    key: "Scab_Stage_1",
-    disease: "Scab leaf disease Stage 1",
-    description: "",
-    description2: "",
-    treatmentPlan: {
-      0: TreatmentPlan(
-        plan: "",
-        desc: "",
-      ),
-      1: TreatmentPlan(
-        plan: "",
-        desc: "",
-      ),
-      2: TreatmentPlan(
-        plan: "",
-        desc: "",
-      ),
-      3: TreatmentPlan(
-        plan: "",
-        desc: "",
-      ),
-      4: TreatmentPlan(
-        plan: "",
-        desc: "",
-      ),
-      5: TreatmentPlan(
-        plan: "",
-        desc: "",
-      ),
-      6: TreatmentPlan(
-        plan: "",
-        desc: "",
-      ),
-      7: TreatmentPlan(
-        plan: "",
-        desc: "",
-      ),
-      8: TreatmentPlan(
-        plan: "",
-        desc: "",
-      ),
-      9: TreatmentPlan(
-        plan: "",
-        desc: "",
-      ),
-    },
-  ),
-  DiseaseModel(
-    key: "Scab_Stage_2",
+    key: "Scab_leaf_disease_Stage_2",
     disease: "Scab leaf disease Stage 2",
-    description: "",
+    description:
+        "In the second stage of scab leaf disease, the symptoms become more pronounced and concerning. Infected leaves exhibit larger and more numerous lesions with a raised, scaly texture. The scabs often take on a dark, olive-green or black appearance, creating a distinctive and unsightly pattern. At this stage, the disease may spread to nearby fruit, increasing the risk of reduced fruit quality and yield. Timely intervention is critical to prevent further escalation of the disease, emphasizing the importance of effective management strategies in scab leaf disease control.",
     description2: "",
     treatmentPlan: {
       0: TreatmentPlan(
-        plan: "",
-        desc: "",
+        plan: "Prune and Remove Affected Leaves",
+        desc:
+            "Continue to prune and remove leaves with advanced scab symptoms. Be thorough in your removal to reduce the disease's source of spores.",
       ),
       1: TreatmentPlan(
-        plan: "",
-        desc: "",
+        plan: "Sanitation",
+        desc:
+            "Maintain a clean orchard environment by collecting and disposing of fallen leaves and any other infected plant material.",
       ),
       2: TreatmentPlan(
-        plan: "",
-        desc: "",
+        plan: "Fungicide Application",
+        desc:
+            "Apply fungicides labeled for scab control following a recommended schedule and dosage. Consult with local experts for specific recommendations based on your region and tree variety.",
       ),
       3: TreatmentPlan(
-        plan: "",
-        desc: "",
+        plan: "Fruit Thinning",
+        desc:
+            "To reduce disease pressure on the fruit, consider thinning the fruit clusters, allowing better air circulation and quicker drying of leaves.",
       ),
       4: TreatmentPlan(
-        plan: "",
-        desc: "",
+        plan: "Proper Pruning",
+        desc:
+            "Continue with selective pruning to improve canopy air circulation, as well as remove branches that are heavily affected by the disease.",
       ),
       5: TreatmentPlan(
-        plan: "",
-        desc: "",
+        plan: "Mulch and Weed Control",
+        desc:
+            "Maintain mulch around the base of trees and continue to control weeds to prevent soil splashing and minimize the risk of spore transfer.",
       ),
       6: TreatmentPlan(
-        plan: "",
-        desc: "",
+        plan: "Monitor and Scout",
+        desc:
+            "Regularly inspect the orchard for new infections, and remove affected leaves and fruit promptly.",
       ),
       7: TreatmentPlan(
-        plan: "",
-        desc: "",
+        plan: "Record Keeping",
+        desc:
+            "Keep detailed records of disease management efforts, including fungicide applications and their effectiveness.",
       ),
       8: TreatmentPlan(
-        plan: "",
-        desc: "",
+        plan: "Post-Harvest Cleanup",
+        desc:
+            "After harvest, ensure thorough orchard cleanup to remove any remaining infected debris.",
       ),
       9: TreatmentPlan(
-        plan: "",
-        desc: "",
+        plan: "Varietal Selection",
+        desc:
+            "Consider planting or grafting scab-resistant varieties for future planting to reduce the risk of disease.",
+      ),
+      10: TreatmentPlan(
+        plan: "Consult with Experts",
+        desc:
+            "Seek advice from local agricultural extension services or horticultural experts for guidance tailored to your specific conditions.",
+      ),
+    },
+  ),
+  DiseaseModel(
+    key: "Blight_leaf_disease_stage_1",
+    disease: "Blight leaf disease stage 1",
+    description:
+        "In the initial stage of blight leaf disease, subtle symptoms start to appear on plant leaves, typically affecting a wide range of crops, including tomatoes and potatoes. Early signs include small, water-soaked lesions or spots that are often brown or black in color and can be mistaken for various other leaf diseases. At this stage, the overall plant health remains relatively unaffected, but it serves as a critical warning sign for vigilant gardeners and farmers. Swift identification and intervention are vital to prevent the disease from progressing to more advanced stages and causing severe damage to the plants.",
+    description2:
+        "A treatment plan for blight leaf disease at stage 1 is essential to prevent the disease from spreading and causing severe damage to your plants. Here's a plan to manage blight leaf disease effectively in its early stages",
+    treatmentPlan: {
+      0: TreatmentPlan(
+        plan: "Prune and Remove Affected Leaves",
+        desc:
+            "Begin by carefully pruning and removing leaves showing early blight symptoms. Dispose of the infected plant material properly, as it can harbor the pathogen.",
+      ),
+      1: TreatmentPlan(
+        plan: "Sanitation",
+        desc:
+            "Maintain good garden hygiene by regularly cleaning up fallen leaves and plant debris, which can harbor the blight pathogen.",
+      ),
+      2: TreatmentPlan(
+        plan: "Fungicide Application",
+        desc:
+            "Consider applying a preventive fungicide treatment labeled for blight control. Consult with local agricultural extension services or experts for recommendations on the most suitable fungicide and application timing for your region and plant type.",
+      ),
+      3: TreatmentPlan(
+        plan: "Water Management",
+        desc:
+            "Water your plants at the base rather than overhead to minimize leaf wetness, which can encourage blight development. Water early in the day to allow foliage to dry before evening.",
+      ),
+      4: TreatmentPlan(
+        plan: "Mulch and Weed Control: ",
+        desc:
+            "Apply mulch around the base of plants to prevent soil splashing onto the leaves, as the pathogen can be soil-borne. Keep the garden area weed-free to reduce the risk of blight spores harboring in nearby plants.",
+      ),
+      5: TreatmentPlan(
+        plan: "Monitor and Scout",
+        desc:
+            "Regularly inspect your plants for any signs of new infections. Early detection is key to prompt action.",
+      ),
+      6: TreatmentPlan(
+        plan: "Record Keeping",
+        desc:
+            "Maintain records of disease incidence, treatment schedules, and their effectiveness for future reference.",
+      ),
+      7: TreatmentPlan(
+        plan: "Crop Rotation",
+        desc:
+            "If possible, practice crop rotation in subsequent seasons to reduce the risk of blight recurrence in the same area.",
+      ),
+      8: TreatmentPlan(
+        plan: "Consult Experts",
+        desc:
+            "Seek advice from local horticultural experts or agricultural extension services for guidance specific to your region and plant variety.",
+      ),
+    },
+  ),
+  DiseaseModel(
+    key: "Blight_leaf_disease_stage 2",
+    disease: "Blight leaf disease stage 2",
+    description:
+        "In the second stage of blight leaf disease, the situation intensifies with more noticeable and destructive symptoms. Infected plant leaves display larger, dark lesions with characteristic concentric rings, often surrounded by a yellow halo. These lesions can rapidly multiply, causing the leaves to wither and die, significantly impacting the plant's overall health and vitality. At this stage, the disease may spread to the plant's fruit or tubers, posing a severe threat to crop yields. Timely intervention and a comprehensive management strategy are imperative to prevent further escalation and mitigate the damage caused by blight leaf disease",
+    description2: "",
+    treatmentPlan: {
+      0: TreatmentPlan(
+        plan: "Prune and Remove Affected Plant Parts",
+        desc:
+            "Continue to prune and remove severely infected leaves, stems, or fruit. Dispose of the infected material away from your garden, and do not compost it.",
+      ),
+      1: TreatmentPlan(
+        plan: "Sanitation",
+        desc:
+            "Maintain excellent garden hygiene by cleaning up fallen leaves and plant debris regularly to prevent overwintering of the pathogen.",
+      ),
+      2: TreatmentPlan(
+        plan: "Fungicide Application",
+        desc:
+            "Apply a broad-spectrum fungicide labeled for blight control, following the recommended schedule and dosage. Consult with local experts for specific recommendations based on your region and plant type.",
+      ),
+      3: TreatmentPlan(
+        plan: "Isolate and Quarantine",
+        desc:
+            "Isolate severely infected plants from healthy ones to prevent the spread of the disease. Consider removing the most heavily affected plants.",
+      ),
+      4: TreatmentPlan(
+        plan: "Water Management",
+        desc:
+            "Continue to water at the base of plants to minimize leaf wetness. Remove any irrigation methods that spray water onto leaves.",
+      ),
+      5: TreatmentPlan(
+        plan: "Mulch and Weed Control",
+        desc:
+            "Maintain mulch and weed control to reduce soil splashing, which can transfer blight spores onto leaves.",
+      ),
+      6: TreatmentPlan(
+        plan: "Harvest Early",
+        desc:
+            "Harvest mature fruit or tubers as soon as possible to salvage what you can before they become severely affected.",
+      ),
+      7: TreatmentPlan(
+        plan: "Crop Rotation",
+        desc:
+            "Practice crop rotation in the next growing season to reduce the risk of blight recurrence in the same area.",
+      ),
+      8: TreatmentPlan(
+        plan: "Monitor and Scout",
+        desc:
+            "Regularly inspect your plants for new infections, and remove affected parts promptly.",
+      ),
+      9: TreatmentPlan(
+        plan: "Record Keeping",
+        desc:
+            "Maintain records of disease management efforts, including fungicide applications and their effectiveness.",
+      ),
+      10: TreatmentPlan(
+        plan: "Consult with Experts",
+        desc:
+            "Seek guidance from local horticultural experts or agricultural extension services for advice tailored to your region and plant variety.",
       ),
     },
   ),
--- lib/utils/static/growth_static.dart
+++ lib/utils/static/growth_static.dart
@@ -7,9 +7,10 @@ List<GrowthModel> growthStatic = [
     desc:
         "The plant's growth is severely below what is expected for its age, and the suggested fertilizer plan is an attempt to address and improve the situation.",
     fertilAmount:
-        "Use fertilizer with N-P-K ratios with 20-20-20.                        (grams per square meter (g/m²), For example, if you want to apply 100 grams of a 20-20-20 fertilizer per square meter, you would apply 20 grams of nitrogen, 20 grams of phosphorus, and 20 grams of potassium.)",
+        "Use fertilizer with N-P-K ratios with 20-20-20.(grams per square meter (g/m²), For example, if you want to apply 100 grams of a 20-20-20 fertilizer per square meter, you would apply 20 grams of nitrogen, 20 grams of phosphorus, and 20 grams of potassium.)",
     fertilTiming: "Fertilize during the active growing season",
-    appFreq: "Apply fertilizer every 2-4 weeks, adjusting per the specific product's recommendations.",
+    appFreq:
+        "Apply fertilizer every 2-4 weeks, adjusting per the specific product's recommendations.",
     appMethod:
         "Water the plant a day or two before applying fertilizer. Apply around the base, avoiding direct contact with stems or leaves.",
     flushWithWater:
@@ -23,51 +24,73 @@ List<GrowthModel> growthStatic = [
     status: "Mildly under grown ",
     desc:
         "The plant's growth is not meeting expectations for its age, and the suggested fertilizer plan is aimed at improving its growth.",
-    fertilAmount: "",
-    fertilTiming: "",
-    appFreq: "",
-    appMethod: "",
-    flushWithWater: "",
-    monitorAndAdjust: "",
-    soilQuality: "",
+    fertilAmount:
+        "Use fertilizer with N-P-K ratios with 14-14-14.(grams per square meter (g/m²), For example, if you want to apply 100 grams of a 14-14-14 fertilizer per square meter, you would apply 14 grams of nitrogen, 14 grams of phosphorus, and 14 grams of potassium.)",
+    fertilTiming: "Fertilize during the active growing season",
+    appFreq:
+        "Apply fertilizer every 3-5 weeks, adjusting per the specific product's recommendations.",
+    appMethod:
+        "Water the anthurium after fertilization to help nutrients reach the root zone and prevent buildup.",
+    flushWithWater:
+        "Water the anthurium after fertilization to help nutrients reach the root zone and prevent buildup.",
+    monitorAndAdjust:
+        "Watch for signs of over-fertilization and adjust the amount and frequency accordingly.",
+    soilQuality: "Ensure well-draining, organic-rich soil for anthuriums.",
   ),
   GrowthModel(
     key: "healthy",
     status: "Healthy",
     desc:
         "The plant's growth is satisfactory for its age. You can use this fertilizer plan to maintain its health.",
-    fertilAmount: "",
-    fertilTiming: "",
-    appFreq: "",
-    appMethod: "",
-    flushWithWater: "",
-    monitorAndAdjust: "",
-    soilQuality: "",
+    fertilAmount:
+        "Use fertilizer with N-P-K ratios with 10-10-10.(grams per square meter (g/m²), For example, if you want to apply 100 grams of a 10-10-10 fertilizer per square meter, you would apply 10 grams of nitrogen, 10 grams of phosphorus, and 10 grams of potassium.)",
+    fertilTiming: "Fertilize during the active growing season",
+    appFreq:
+        "Apply fertilizer every 4-6 weeks, adjusting per the specific product's recommendations.",
+    appMethod:
+        "Water the plant a day or two before applying fertilizer. Apply around the base, avoiding direct contact with stems or leaves.",
+    flushWithWater:
+        "Water the anthurium after fertilization to help nutrients reach the root zone and prevent buildup.",
+    monitorAndAdjust:
+        "Watch for signs of over-fertilization and adjust the amount and frequency accordingly.",
+    soilQuality: "Ensure well-draining, organic-rich soil for anthuriums.",
   ),
   GrowthModel(
     key: "midly_undergrown",
     status: "Mildly over grown ",
     desc:
         "The plant's growth has exceeded the expected size for its age. It may benefit from a lighter fertilizer plan to maintain its current size without promoting excessive growth.",
-    fertilAmount: "",
-    fertilTiming: "",
-    appFreq: "",
-    appMethod: "",
-    flushWithWater: "",
-    monitorAndAdjust: "",
-    soilQuality: "",
+    fertilAmount:
+        "Use a balanced, slow-release fertilizer with lower N-P-K ratios, such as 10-5-5 (grams per square meter (g/m²), e.g., for 100 grams of fertilizer, apply 10 grams of nitrogen, 5 grams of phosphorus, and 5 grams of potassium).",
+    fertilTiming:
+        "Fertilize during the active growing season, but reduce the frequency to every 8-10 weeks.",
+    appFreq:
+        "Apply fertilizer every 8-10 weeks, adjusting per the specific product's recommendations.",
+    appMethod:
+        "Water the plant a day or two before applying fertilizer. Apply around the base, avoiding direct contact with stems or leaves.",
+    flushWithWater:
+        "Water the anthurium after fertilization to help nutrients reach the root zone and prevent buildup.",
+    monitorAndAdjust:
+        "Keep an eye on the plant's growth and adjust the amount and frequency accordingly.",
+    soilQuality: "Ensure well-draining, organic-rich soil for anthuriums.",
   ),
   GrowthModel(
     key: "critically_overgrown",
     status: "Critically  over grown ",
     desc:
         "The plant's growth is severely below what is expected for its age, and the suggested fertilizer plan is an attempt to address and improve the situation.",
-    fertilAmount: "",
-    fertilTiming: "",
-    appFreq: "",
-    appMethod: "",
-    flushWithWater: "",
-    monitorAndAdjust: "",
-    soilQuality: "",
+    fertilAmount:
+        "Use a very low nitrogen, slow-release fertilizer with N-P-K ratios like 5-5-5 (grams per square meter (g/m²), e.g., for 100 grams of fertilizer, apply 5 grams of nitrogen, 10 grams of phosphorus, and 10 grams of potassium).",
+    fertilTiming:
+        "Fertilize during the active growing season, but reduce the frequency to every 12-16 weeks.",
+    appFreq:
+        "Apply fertilizer every 12-16 weeks, adjusting per the specific product's recommendations.",
+    appMethod:
+        "Water the plant a day or two before applying fertilizer. Apply around the base, avoiding direct contact with stems or leaves.",
+    flushWithWater:
+        "Water the anthurium after fertilization to help nutrients reach the root zone and prevent buildup.",
+    monitorAndAdjust:
+        "Keep a close watch on the plant's growth and adjust the amount and frequency accordingly.",
+    soilQuality: "Ensure well-draining, organic-rich soil for anthuriums.",
   ),
 ];
