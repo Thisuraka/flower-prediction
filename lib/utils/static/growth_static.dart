0129c0f8ad2b552e94cccf5e9ea967ddc212edb2
Thisuraka <vibudathisuraka@email.com>
2023-10-30 12:21:52 +0530

    minor bug fix

--- lib/utils/static/growth_static.dart
+++ lib/utils/static/growth_static.dart
@@ -9,8 +9,7 @@ List<GrowthModel> growthStatic = [
     fertilAmount:
         "Use fertilizer with N-P-K ratios with 20-20-20.(grams per square meter (g/m²), For example, if you want to apply 100 grams of a 20-20-20 fertilizer per square meter, you would apply 20 grams of nitrogen, 20 grams of phosphorus, and 20 grams of potassium.)",
     fertilTiming: "Fertilize during the active growing season",
-    appFreq:
-        "Apply fertilizer every 2-4 weeks, adjusting per the specific product's recommendations.",
+    appFreq: "Apply fertilizer every 2-4 weeks, adjusting per the specific product's recommendations.",
     appMethod:
         "Water the plant a day or two before applying fertilizer. Apply around the base, avoiding direct contact with stems or leaves.",
     flushWithWater:
@@ -27,8 +26,7 @@ List<GrowthModel> growthStatic = [
     fertilAmount:
         "Use fertilizer with N-P-K ratios with 14-14-14.(grams per square meter (g/m²), For example, if you want to apply 100 grams of a 14-14-14 fertilizer per square meter, you would apply 14 grams of nitrogen, 14 grams of phosphorus, and 14 grams of potassium.)",
     fertilTiming: "Fertilize during the active growing season",
-    appFreq:
-        "Apply fertilizer every 3-5 weeks, adjusting per the specific product's recommendations.",
+    appFreq: "Apply fertilizer every 3-5 weeks, adjusting per the specific product's recommendations.",
     appMethod:
         "Water the anthurium after fertilization to help nutrients reach the root zone and prevent buildup.",
     flushWithWater:
@@ -45,8 +43,7 @@ List<GrowthModel> growthStatic = [
     fertilAmount:
         "Use fertilizer with N-P-K ratios with 10-10-10.(grams per square meter (g/m²), For example, if you want to apply 100 grams of a 10-10-10 fertilizer per square meter, you would apply 10 grams of nitrogen, 10 grams of phosphorus, and 10 grams of potassium.)",
     fertilTiming: "Fertilize during the active growing season",
-    appFreq:
-        "Apply fertilizer every 4-6 weeks, adjusting per the specific product's recommendations.",
+    appFreq: "Apply fertilizer every 4-6 weeks, adjusting per the specific product's recommendations.",
     appMethod:
         "Water the plant a day or two before applying fertilizer. Apply around the base, avoiding direct contact with stems or leaves.",
     flushWithWater:
@@ -56,22 +53,19 @@ List<GrowthModel> growthStatic = [
     soilQuality: "Ensure well-draining, organic-rich soil for anthuriums.",
   ),
   GrowthModel(
-    key: "midly_undergrown",
+    key: "midly_overgrown",
     status: "Mildly over grown ",
     desc:
         "The plant's growth has exceeded the expected size for its age. It may benefit from a lighter fertilizer plan to maintain its current size without promoting excessive growth.",
     fertilAmount:
         "Use a balanced, slow-release fertilizer with lower N-P-K ratios, such as 10-5-5 (grams per square meter (g/m²), e.g., for 100 grams of fertilizer, apply 10 grams of nitrogen, 5 grams of phosphorus, and 5 grams of potassium).",
-    fertilTiming:
-        "Fertilize during the active growing season, but reduce the frequency to every 8-10 weeks.",
-    appFreq:
-        "Apply fertilizer every 8-10 weeks, adjusting per the specific product's recommendations.",
+    fertilTiming: "Fertilize during the active growing season, but reduce the frequency to every 8-10 weeks.",
+    appFreq: "Apply fertilizer every 8-10 weeks, adjusting per the specific product's recommendations.",
     appMethod:
         "Water the plant a day or two before applying fertilizer. Apply around the base, avoiding direct contact with stems or leaves.",
     flushWithWater:
         "Water the anthurium after fertilization to help nutrients reach the root zone and prevent buildup.",
-    monitorAndAdjust:
-        "Keep an eye on the plant's growth and adjust the amount and frequency accordingly.",
+    monitorAndAdjust: "Keep an eye on the plant's growth and adjust the amount and frequency accordingly.",
     soilQuality: "Ensure well-draining, organic-rich soil for anthuriums.",
   ),
   GrowthModel(
@@ -83,8 +77,7 @@ List<GrowthModel> growthStatic = [
         "Use a very low nitrogen, slow-release fertilizer with N-P-K ratios like 5-5-5 (grams per square meter (g/m²), e.g., for 100 grams of fertilizer, apply 5 grams of nitrogen, 10 grams of phosphorus, and 10 grams of potassium).",
     fertilTiming:
         "Fertilize during the active growing season, but reduce the frequency to every 12-16 weeks.",
-    appFreq:
-        "Apply fertilizer every 12-16 weeks, adjusting per the specific product's recommendations.",
+    appFreq: "Apply fertilizer every 12-16 weeks, adjusting per the specific product's recommendations.",
     appMethod:
         "Water the plant a day or two before applying fertilizer. Apply around the base, avoiding direct contact with stems or leaves.",
     flushWithWater:
